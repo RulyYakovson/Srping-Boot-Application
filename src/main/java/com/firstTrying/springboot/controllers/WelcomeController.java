7c2fd99c6e29c22d45aabf317764fc71060504d2
RulyYakovson <sariruli@gmail.com>
2018-10-28 15:10:00 +0200

    CheckURL

--- src/main/java/com/firstTrying/springboot/controllers/WelcomeController.java
+++ src/main/java/com/firstTrying/springboot/controllers/WelcomeController.java
@@ -2,7 +2,9 @@ package com.firstTrying.springboot.controllers;
 
 import com.firstTrying.springboot.services.WelcomeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -15,6 +17,11 @@ public class WelcomeController {
     @RequestMapping("welcome")
     public String welcomeMessage(){
         return service.returnWelcomMessage();
+    }
+
+    @RequestMapping(value = "/url/{urlToValidate}", method = RequestMethod.GET)
+    public boolean checkURL(@PathVariable("urlToValidate") String url){
+        return service.checkURL(url);
     }
 
 }
--- src/main/java/com/firstTrying/springboot/services/WelcomeService.java
+++ src/main/java/com/firstTrying/springboot/services/WelcomeService.java
@@ -1,11 +1,36 @@
 package com.firstTrying.springboot.services;
 
-//import org.springframework.stereotype.Service;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.HttpURLConnection;
+
+@Service
 public class WelcomeService {
     public String returnWelcomMessage(){
-        return "Good Morning To You !! ";
+        return "Good Morning !! ";
+    }
+
+    public boolean checkURL(String urlString) {
+        HttpURLConnection huc = null;
+        int response = 0;
+        try {
+            urlString = urlString.replace('a', '.');
+            //urlString = "https://www.google.co.il/";
+            URL u = new URL(urlString);
+            huc = (HttpURLConnection) u.openConnection();
+            huc.setRequestMethod("GET");
+            huc.connect();
+            response = huc.getResponseCode();
+        } catch (MalformedURLException e) {
+
+        } catch (IOException e) {
+
+        }
+            return response == 200;
+
     }
 }
